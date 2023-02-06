b46a9f96fbdf60e97aae350bd5cacab64c20fbf2
SubVray <66286368+SubVray@users.noreply.github.com>
2023-02-06 16:16:28 -0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const cors = require("cors");
 
 const userRoute = require("./src/routes/users.route");
 
@@ -8,6 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
+app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
