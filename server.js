2ce5e59d048341ef55f482114d5aabd50db2dab9
SubVray <66286368+SubVray@users.noreply.github.com>
2023-02-05 23:29:01 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,44 +1,44 @@
-const uri = process.env.MONGODB_URI || 5000;
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
+// const uri = process.env.MONGODB_URI || 5000;
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const app = express();
 
-// Parse the incoming request body as JSON
-app.use(express.json());
+// // Parse the incoming request body as JSON
+// app.use(express.json());
 
-// Connect to MongoDB
-mongoose
-  .connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Successfully connected to MongoDB."))
-  .catch((error) => console.error(error));
+// // Connect to MongoDB
+// mongoose
+//   .connect(uri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("Successfully connected to MongoDB."))
+//   .catch((error) => console.error(error));
 
-// Define a Mongoose schema for our data
-const dataSchema = new mongoose.Schema({
-  phoneNumber: String,
-  numCedula: String,
-  firma: String,
-  frontImg: String,
-  backImg: String,
-});
+// // Define a Mongoose schema for our data
+// const dataSchema = new mongoose.Schema({
+//   phoneNumber: String,
+//   numCedula: String,
+//   firma: String,
+//   frontImg: String,
+//   backImg: String,
+// });
 
-// Compile the schema into a Mongoose model
-const Data = mongoose.model("Data", dataSchema);
+// // Compile the schema into a Mongoose model
+// const Data = mongoose.model("Data", dataSchema);
 
-// Define an endpoint for saving data to MongoDB
-app.post("/data", (req, res) => {
-  const newData = new Data({
-    key: req.body.key,
-    value: req.body.value,
-  });
+// // Define an endpoint for saving data to MongoDB
+// app.post("/data", (req, res) => {
+//   const newData = new Data({
+//     key: req.body.key,
+//     value: req.body.value,
+//   });
 
-  newData
-    .save()
-    .then(() => res.status(201).send("Data saved successfully."))
-    .catch((error) => res.status(500).send(error));
-});
+//   newData
+//     .save()
+//     .then(() => res.status(201).send("Data saved successfully."))
+//     .catch((error) => res.status(500).send(error));
+// });
 
-// Start the server
-app.listen(5000, () => console.log("Server listening on port 5000."));
+// // Start the server
+// app.listen(5000, () => console.log("Server listening on port 5000."));
