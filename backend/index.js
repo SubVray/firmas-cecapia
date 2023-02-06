9428e0346d11722f808ae4c43929dff146934954
SubVray <66286368+SubVray@users.noreply.github.com>
2023-02-06 14:38:38 -0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json());
 app.use("/api", userRoute);
 
