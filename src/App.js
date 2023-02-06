d4073ee92cf6de48ad5fd536a0befac52fd2a1cf
SubVray <66286368+SubVray@users.noreply.github.com>
2023-02-05 22:00:03 -0600

    cecapia

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
     /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
       navigator.userAgent
     );
+  const signatureCanvas = useRef(null);
   const [isBack, setIsBack] = useState(false);
   const [forntImg, setFrontImg] = useState("");
   const [bakcImg, setBackImg] = useState("");
@@ -33,6 +34,18 @@ function App() {
       setCamera(videoConstraints);
     }
   }
+  const saveImage = () => {
+    const signature = signatureCanvas.current
+      .getTrimmedCanvas()
+      .toDataURL("image/png");
+    signatureCanvas.current.off();
+    // Guardar la imagen usando tu método preferido, como por ejemplo enviarla a un servidor o descargarla en el cliente
+  };
+  const deleteFirma = () => {
+    signatureCanvas.current.clear();
+    signatureCanvas.current.on();
+    // Guardar la imagen usando tu método preferido, como por ejemplo enviarla a un servidor o descargarla en el cliente
+  };
 
   const webcamRef = useRef(null);
   const capture = () => {
@@ -40,17 +53,45 @@ function App() {
     if (!isBack) {
       setFrontImg(imageSrc);
       setIsBack(true);
+      MySwal.fire({
+        title: "success",
+        text: "Foto frontal exitosa!",
+        icon: "success",
+        showCancelButton: false,
+        confirmButtonColor: "#3085d6",
+        confirmButtonText: "Entendido",
+      }).then(() => {
+        document.getElementById("switch-button").click();
+      });
     } else {
       setBackImg(imageSrc);
       setIsBack(false);
+      Swal.fire({
+        title: "success",
+        text: "Foto posterior exitosa!",
+        icon: "success",
+        showCancelButton: false,
+        confirmButtonColor: "#3085d6",
+        confirmButtonText: "Entendido",
+      }).then(() => {
+        document.getElementById("section-cedula").classList.add("d-none");
+        document.getElementById("btn-send-photos").classList.add("d-none");
+        document.getElementById("btn-send-cecapia").classList.remove("d-none");
+        document.getElementById("btn-camara-back").classList.remove("d-none");
+      });
     }
+  };
+  const handleCamBack = () => {
+    document.getElementById("btn-send-cecapia").classList.add("d-none");
+    document.getElementById("btn-camara-back").classList.add("d-none");
+    document.getElementById("btn-send-photos").classList.remove("d-none");
   };
 
   return (
     <div className="">
       <form
         action=""
-        className="form-control d-flex justify-content-center align-items-center flex-column ">
+        className="form-control d-flex justify-content-center align-items-center flex-column m-auto ">
         <div className="logo-container">
           <img src={logo} alt="Logo" width={"250px"} />
         </div>
@@ -58,8 +99,8 @@ function App() {
           <label htmlFor="text-phone-number">Numero de teléfono:</label>
           <input type="text" className="form-control" id="text-phone-number" />
           <div className=" d-flex justify-content-start align-items-center gap-1">
-            <label htmlFor="text-phone-number"># cédula:</label>
-            <select name="" id="" className="form-select w-50">
+            <label htmlFor="text-phone-number"># Cédula:</label>
+            <select name="" id="" className="form-select w-75">
               <option value="">Cédula de identidad</option>
               <option value="">Cédulas jurídicas</option>
               <option value="">DIMEX</option>
@@ -71,6 +112,7 @@ function App() {
           </label>
           <SignatureCanvas
             penColor="blue"
+            ref={signatureCanvas}
             canvasProps={{
               maxwidth: 300,
               height: 300,
@@ -80,20 +122,42 @@ function App() {
           />
         </div>
         <div className="d-flex gap-2 my-2">
-          <button type="button" className="btn btn-success text-center">
+          <button
+            type="button"
+            onClick={saveImage}
+            className="btn btn-success text-center">
             Guardar
           </button>
-          <button type="button" className="btn btn-danger text-center">
+          <button
+            type="button"
+            onClick={deleteFirma}
+            className="btn btn-danger text-center">
             Borrar
           </button>
         </div>
         <div className="">
           <button
             type="button"
+            id="btn-send-photos"
             onClick={detectDeviceType}
             className="btn btn-primary">
             Subir fotos de la cédula
           </button>
+          <div className=" d-flex gap-2">
+            <button
+              type="button"
+              id="btn-send-cecapia"
+              className="btn btn-primary d-none">
+              Enviar a Cecapia
+            </button>
+            <button
+              type="button"
+              id="btn-camara-back"
+              onClick={handleCamBack}
+              className="btn btn-dark d-none">
+              📷
+            </button>
+          </div>
         </div>
         <div id="section-cedula" className="d-none">
           <div className="">
@@ -129,7 +193,7 @@ function App() {
           </button>
         </div>
       </form>
-      <div className="my-3">
+      <div className="my-3 d-flex gap-2">
         <img src={forntImg} id="img1" className="w-25" alt="" />
         <img src={bakcImg} id="img2" className="w-25" alt="" />
       </div>
