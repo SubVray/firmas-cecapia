5d4cf686655309d0dd8f23602ea5e828b06fb175
SubVray <66286368+SubVray@users.noreply.github.com>
2023-02-06 00:28:22 -0600

    cecapia

--- frontend/server.js
+++ frontend/server.js
@@ -1,19 +1,17 @@
+const uri = process.env.MONGODB_URI || 5000;
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
 // Parse the incoming request body as JSON
 app.use(express.json());
-mongoose.set("strictQuery", false);
+
 // Connect to MongoDB
 mongoose
-  .connect(
-    "mongodb+srv://SubVray:jim123@cluster0.6puj5ox.mongodb.net/?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Successfully connected to MongoDB."))
   .catch((error) => console.error(error));
 
